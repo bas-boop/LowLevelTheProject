d52ecab14211d4fd61cd10479c72d65225430c68
Bas de Reus <70578065+bas-boop@users.noreply.github.com>
2025-09-23 15:11:24 +0200

    Added texture class for big 5

---
+++ src/ChatClient/Texture.cpp
@@ -0,0 +1,36 @@
+﻿#include "Texture.h"
+
+Texture::Texture()
+{
+    
+}
+
+Texture::~Texture()
+{
+    
+}
+
+Texture::Texture(const Texture& other)
+{
+    this->data = new int[size];
+    std::copy(other.data, other.size + other.data, data);
+}
+
+Texture& Texture::operator=(Texture& other)
+{
+    if (this == &other) return *this;
+
+    delete[] data;
+
+    this->name = other.name;
+    this->size = other.size;
+    this->data = other.data;
+    std::copy(other.data, other.size + other.data, data);
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream& os, const Texture& t)
+{
+    std::cout << t.name << " " << &t << " " << t.size << '\n';
+    return os;
+}
---
+++ src/ChatClient/Texture.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <iostream>
+
+class Texture
+{
+public:
+    Texture();
+    virtual ~Texture();
+    Texture (const Texture& other);
+    Texture& operator=(Texture& other);
+
+    std::string name;
+    int size = 2;
+    int* data;
+
+    friend std::ostream& operator <<(std::ostream& os, const Texture& t);
+};
